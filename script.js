69db390e8ca6664c523072acca00c4157b49936c
MartinaVenasco <martinavenasco@gmail.com>
2022-07-18 19:09:33 +0200

    last commit

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-console.log('Hello')
-const min = require('lodash/min')
-console.log(min([34,352,52,34,2,4,123131,231,321,24,3,4,235,34,5,345,345]))
+// console.log('Hello')
+
+// const min = require('lodash/min')
+// console.log(min([34,352,52,34,2,4,123131,231,321,24,3,4,235,34,5,345,345]))
+import.meta.webpack
+import _ from 'lodash';
+
+// const _= require ('lodash')
+// const { min } = require('lodash')
+// const prova =[34,352,52,34,2,4,123131,231,321,24,3,4,235,34,5,345,345]
+
+// const test1 = min(prova)
+// console.log(test1)
+function component() {
+    const element = document.createElement('div');
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    return element;
+  }
+
+  document.body.appendChild(component());
